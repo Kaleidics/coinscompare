786721681ac79ef2e72043e9c4a42fa4399ca7b6
Eddie Chu <contact@eddiechu.me>
2019-03-16 00:25:00 -0700

    Updates

---
+++ precache-manifest.4c8ae4680fea8a6178fdc59d03cc3c9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7918550130b8ccb347ce",
+    "url": "/coinscompare/static/js/runtime~main.1cb81ce3.js"
+  },
+  {
+    "revision": "ddf450786445db715036",
+    "url": "/coinscompare/static/js/main.7904a8a4.chunk.js"
+  },
+  {
+    "revision": "50dba484434085c7a44a",
+    "url": "/coinscompare/static/js/2.f11a5055.chunk.js"
+  },
+  {
+    "revision": "ddf450786445db715036",
+    "url": "/coinscompare/static/css/main.8cd02021.chunk.css"
+  },
+  {
+    "revision": "0584fc05821b4ab7049e4214b6286ea8",
+    "url": "/coinscompare/index.html"
+  }
+];
