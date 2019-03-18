1aacb43bce620e1a39769e6e05174d49cbfb24d3
Eddie Chu <contact@eddiechu.me>
2019-03-17 17:28:10 -0700

    update files

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ParticlesBg from './ParticlesBg';
+import CoinList from './CoinList';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <header id = "title">
+                    <ParticlesBg />
+                </header>
+
+                <CoinList />
+            </div>
+        )
+    }
+}
---
+++ src/components/CoinList.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import {API_BASE_URL} from '../config';
+import {API_BASE_IMAGE} from '../config';
+
+
+export default class Coins extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            lists: [],
+            error: null,
+        };
+    }
+
+    componentDidMount() {
+        this.loadCoins();
+    }
+
+    loadCoins() {
+        this.setState({
+            error: null,
+            loading: true
+        });
+        return fetch(`${API_BASE_URL}?limit=100&tsym=USD`)
+        .then(res => {
+            if(!res.ok) {
+                return Promise.reject(res.statusText);
+            }
+            return res.json();
+        })
+        .then(coins => {
+            console.log('coins:', coins);
+            this.setState({
+                lists: coins.Data,
+                loading: false
+            })
+        }
+        )
+        .catch(err => {
+            this.setState({
+                error: 'Could not load coins',
+                loading: false
+            })
+        });
+    }
+
+    render() {
+        const coins = this.state.lists.map(coin => {
+            return <ul key={coin.CoinInfo.Id}>
+            <li><img className='coinImages' src={`${API_BASE_IMAGE}${coin.CoinInfo.ImageUrl}`} /></li>
+            <li>{coin.CoinInfo.FullName}</li>
+            <li>{coin.CoinInfo.FullName}</li>
+            <li>{coin.CoinInfo.FullName}</li>
+            <li>{coin.CoinInfo.FullName}</li>
+            </ul>
+        });
+
+        return (
+        <div>
+            {coins}  
+        </div>
+        );
+    }
+    
+}
