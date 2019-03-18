e19cf3dbfcc1d738fca56ef4ef956087a27bd708
Eddie Chu <contact@eddiechu.me>
2019-03-17 20:48:05 -0700

    update files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
-import ParticlesBg from './components/ParticlesBg';
+import App from './components/App';
+// import ParticlesBg from './components/ParticlesBg';
+// import CoinList from './components/CoinList';
 
-ReactDOM.render(<ParticlesBg />, document.getElementById('title'));
+// ReactDOM.render(<ParticlesBg />, document.getElementById('title'));
 ReactDOM.render(<App />, document.getElementById('root'));
-
+//
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
