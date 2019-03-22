5494d076ecc7b2eee60eba045b35c2c46196dc71
Eddie Chu <contact@eddiechu.me>
2019-03-21 21:52:19 -0700

    update search placeholder reflect functionaility

--- src/components/Search-form.js
+++ src/components/Search-form.js
@@ -5,7 +5,7 @@ export default function SearchForm(props) {
     return (
         <form onSubmit={e => e.preventDefault()}>
             <label htmlFor="search">Search</label>
-            <input type="search" id="search" name="search" placeholder="Search by coin symbol"
+            <input type="search" id="search" name="search" placeholder="Search by coin symbol or name"
                 onChange={e => props.onChange(e.target.value)} />
         </form>
     );
