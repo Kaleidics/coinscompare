404e0e1cccf10bc8edc3a6e87d9f2082c6787a7a
Eddie Chu <contact@eddiechu.me>
2019-03-23 23:59:40 -0700

    debug width issue

--- src/components/ParticlesBg.js
+++ src/components/ParticlesBg.js
@@ -12,10 +12,10 @@ export default function ParticlesBg(props) {
             <Particles
                 params={particleOpt}
             />
-            <div className='app-title'>
+            <section className='app-title'>
             <h2 className='title-h2'>Coins <br/> Compare</h2>
             <p className='title-tagline'>A quick look at imaginary money. Wait, all money is imaginary . . .</p>
-            </div>
+            </section>
         </div>
     )
 }
