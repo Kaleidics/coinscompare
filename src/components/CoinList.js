beb7e5e61426725fed6bdea3b13ab7899ba4ce87
Eddie Chu <42883353+Kaleidics@users.noreply.github.com>
2019-03-23 20:36:01 -0700

    Refine filter for lowercase input

--- src/components/CoinList.js
+++ src/components/CoinList.js
@@ -60,7 +60,7 @@ export default class Coins extends React.Component {
                 coin.name.toLowerCase().includes(this.state.searchTerm.toLowerCase())
             );
             console.log(displayData);
-            if(displayData[0].name.toLowerCase() === this.state.searchTerm) {
+            if(displayData[0].name.toLowerCase() === this.state.searchTerm.toLowercase()) {
                 console.log('here', displayData)
                 displayData = displayData.slice(0,1);
             }
