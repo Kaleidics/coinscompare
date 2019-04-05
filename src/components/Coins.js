c971cf64d3d5c92ba1cf0b538e8111dfb92d1553
Eddie Chu <contact@eddiechu.me>
2019-04-04 21:26:02 -0700

    update files

--- src/components/Coins.js
+++ src/components/Coins.js
@@ -9,6 +9,10 @@ function tidyUp(num) {
 export default function Coin(props) {
     
     const coins = props.data.map((coin, index) => {
+
+       
+
+
         return (
             <ul className='coin-containers' index={index} key={coin.id}>
                 <li><img className='coinImages' src={`${coin.image}`} alt={coin.name} /></li>
@@ -17,8 +21,8 @@ export default function Coin(props) {
                 <li className='ticker'>{coin.symbol}</li>
                 <li className='price'>${tidyUp(coin.current_price)}</li>
                 <li className='totalvol'>$ {tidyUp(coin.total_volume)}</li>
-                <li>$ {tidyUp(coin.market_cap)}</li>
-                <li>24HR</li>
+                <li className='marketcap'>$ {tidyUp(coin.market_cap)}</li>
+                <li className='hour'>24HR</li>
                 <li style={{ color: coin.market_cap_change_percentage_24h >= 0 ? '#7AC27A' : '#FF6060' }}>{(coin.market_cap_change_percentage_24h >= 0 ? '+' : '')}{tidyUp(coin.market_cap_change_percentage_24h)}%</li>
             </ul>
         )
