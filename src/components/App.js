e107cb0c656c0a5c9cdaedc6ff03e93f04081fbf
Eddie <42883353+Kaleidics@users.noreply.github.com>
2019-04-05 23:50:57 -0700

    update sass

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ParticlesBg from './ParticlesBg';
 import CoinList from './CoinList';
+import './CoinList.scss';
+import './Coins.scss';
+import './Loader.scss';
+import './ParticlesBg.scss';
+import './Search-form.scss';
+
 
 export default class App extends React.Component {
     render() {
